619c125c864ae2b4a81bb393440423b214165388
Alex Kerr <alex9000@gmail.com>
2017-07-22 13:42:09 -0700

    feat: display data from BooksAPI and pass to ListBooks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+/*global state*/
+import React, { Component } from 'react'
 //import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -13,7 +14,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+      console.log(this.state.books)
+    })
   }
 
   render() {
@@ -22,7 +31,7 @@ class BooksApp extends React.Component {
         {this.state.showSearchPage ? (
           <SearchBooks />
         ) : (
-          <ListBooks />
+          <ListBooks books={this.state.books}/>
         )}
       </div>
     )
