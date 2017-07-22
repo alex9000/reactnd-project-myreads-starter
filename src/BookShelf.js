21cab772124f6eaec84bdd43e4375f0509ad662a
Alex Kerr <alex9000@gmail.com>
2017-07-22 13:50:02 -0700

    feat: list books in BookShelf and add PropTypes for books

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 const BookShelf = (props) => (
   <div className="bookshelf">
-    <h2 className="bookshelf-title">{props.title}</h2>
+    <h2 className="bookshelf-title">{props.shelf}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        <li>
+        {props.books.map(book => <li>
           <Book
-            title="To Kill a Mockingbird"
-            author="Harper Lee"
-            imageWidth="128px"
-            imageHeight="193px"
-            imageSrc="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"/>
+          title={book.title}
+          author={book.authors[0]}
+          imageWidth="128px"
+          imageHeight="193px"
+          imageSrc={book.imageLinks.smallThumbnail}/>
         </li>
-        <li>
-          <Book
-            title="Ender's Game"
-            author="Orson Scott Card"
-            imageWidth="128px"
-            imageHeight="188px"
-            imageSrc="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"/>
-        </li>
+)}
+
       </ol>
     </div>
   </div>
 );
-
+BookShelf.PropTypes = {
+  books: PropTypes.array.isRequired,
+}
 
 export default BookShelf
