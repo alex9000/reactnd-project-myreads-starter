3b1b8f478d49f3345761b32a96f5e7b1901c52b5
Alex Kerr <alex9000@gmail.com>
2017-07-23 12:35:13 -0700

    refactor: convert BookShelf into class based component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Book from './Book'
 import PropTypes from 'prop-types'
 
-const BookShelf = (props) => (
-  <div className="bookshelf">
-    <h2 className="bookshelf-title">{props.shelf}</h2>
-    <div className="bookshelf-books">
-      <ol className="books-grid">
-        {props.books.map(book => <li>
-          <Book
-          title={book.title}
-          author={book.authors[0]}
-          imageWidth="128px"
-          imageHeight="193px"
-          imageSrc={book.imageLinks.smallThumbnail}/>
-        </li>
-)}
+class BookShelf extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired
+  }
+  render() {
+    const {books, bookShelfTitle} = this.props
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{bookShelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => <li key={book.id}>
+              <Book title={book.title} author={book.authors[0]} imageWidth="128px" imageHeight="193px" imageSrc={book.imageLinks.smallThumbnail} shelf={book.shelf}/>
+            </li>)}
 
-      </ol>
-    </div>
-  </div>
-);
-BookShelf.PropTypes = {
-  books: PropTypes.array.isRequired,
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default BookShelf
