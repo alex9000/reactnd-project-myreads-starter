91f67a8776a6513132566ec2cc13bd7734bd0a5d
Alex Kerr <alex9000@gmail.com>
2017-08-02 13:15:12 -0700

    feat: implement search view
    Add PropTypes for onUpdateBook.
    Implement event handler for select onChange event.
    Call BooksAPI.search to display search results.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,20 +1,62 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
-
-//import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+import PropTypes from 'prop-types'
 
 class SearchBooks extends Component {
+  static PropTypes = {
+    onUpdateBook: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: '',
+    books: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query})
+  }
+
+  componentDidUpdate() {
+    //console.log(this.state)
+    if (this.state.query ) {
+      BooksAPI.search(this.state.query, 5).then((books) => {
+        console.log(books)
+        if (books.error !== 'empty query') {
+          this.setState({books})
+        }
+      })
+    }
+  }
+
   render() {
-    return(
+    const {query, books} = this.state
+    const {onUpdateBook} = this.props
+  //  console.log(query)
+    return (
       <div className="search-books">
         <div className="search-books-bar">
-            <Link to='/' className="close-search">Close</Link>
+          <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author" value={query} onChange={(event) => this.updateQuery(event.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  title={book.title}
+                  author={book.author}
+                  imageSrc={book.imageLinks.smallThumbnail}
+                  imageWidth="128px"
+                  imageHeight="193px"
+                  onUpdateBook={onUpdateBook}/>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
