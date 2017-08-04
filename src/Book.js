debefc5d823ddd6a375e66cb295c532c1a1ddf01
Alex Kerr <alex9000@gmail.com>
2017-08-03 22:59:33 -0700

    refactor: add semicolons to Book & remove redundant select option

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class Book extends Component {
   static PropTypes = {
@@ -16,19 +16,18 @@ class Book extends Component {
   }
 
   handleChange = (e) => {
-    this.props.onUpdateBook(this.props.book, e.target.value)
+    this.props.onUpdateBook(this.props.book, e.target.value);
   }
   // TODO: render multiple authors
 
   render() {
-    const { title, author, imageWidth, imageHeight, shelf, imageSrc } = this.props
+    const { title, author, imageWidth, imageHeight, shelf, imageSrc } = this.props;
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: `${imageWidth}`, height: `${imageHeight}`, backgroundImage: `url(${imageSrc})` }}></div>
             <div className="book-shelf-changer">
               <select value={shelf} onChange={this.handleChange}>
-                <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
@@ -39,8 +38,8 @@ class Book extends Component {
         <div className="book-title">{title}</div>
         <div className="book-authors">{author}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Book
+export default Book;
