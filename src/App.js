b270ff19a31665fa37d2759e40abdd854946045d
Alex Kerr <alex9000@gmail.com>
2017-08-04 08:49:12 -0700

    feat: show selections from search page on main page
    refactor updateBooks() to call BooksAPI.getAll() in order
    to display selected books from search page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, {Component} from 'react'
-import ListBooks from './ListBooks'
-import SearchBooks from './SearchBooks'
-import * as BooksAPI from './BooksAPI'
-import {Route} from 'react-router-dom'
-import './App.css'
+import React, {Component} from 'react';
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
+import * as BooksAPI from './BooksAPI';
+import {Route} from 'react-router-dom';
+import './App.css';
 
 class BooksApp extends Component {
   state = {
-    books: []
+    books: [],
+    bookShelves: []
   }
 
+  /**
+  * @description get all books
+  */
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
+      this.setState({books});
+    });
   }
 
   /**
@@ -22,27 +26,23 @@ class BooksApp extends Component {
   * @param {string} shelf - The new shelf
   */
   updateBook = (updatedBook, shelf) => {
-    BooksAPI.update(updatedBook, shelf).then(() => {
-      this.setState({
-        books: this.state.books.map((b) => {
-          if (b.id === updatedBook.id) {
-            b.shelf = shelf
-          }
-          return b
-        })
-      })
-    })
+    BooksAPI.update(updatedBook, shelf).then((shelves) => {
+      console.log(shelves);
+      BooksAPI.getAll().then((books) => {
+        this.setState({books});
+      });
+    });
   }
 
   render() {
-    const { books } = this.state
+    const { books} = this.state;
     return (
       <div className="app">
         <Route exact path='/' render={() => (<ListBooks books={books} onUpdateBook={this.updateBook} />)}/>
-        <Route path='/search' render={() => (<SearchBooks  onUpdateBook={this.updateBook} />)}/>
+        <Route path='/search' render={() => (<SearchBooks books={books} onUpdateBook={this.updateBook} />)}/>
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
