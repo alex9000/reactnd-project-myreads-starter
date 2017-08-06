762f140fec3108133c70ce51f3ba2e61f9ecf3d6
Alex Kerr <alex9000@gmail.com>
2017-08-06 13:57:29 -0700

    fix: remove console log statement

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class BooksApp extends Component {
   */
   updateBook = (updatedBook, shelf) => {
     BooksAPI.update(updatedBook, shelf).then((shelves) => {
-      console.log(shelves);
       BooksAPI.getAll().then((books) => {
         this.setState({books});
       });
